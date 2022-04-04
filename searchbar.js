9eafbc844f6d26b961b34314a344a4645c4f78a1
MaylisL <mlaporte2345@gmail.com>
2022-04-04 23:36:18 +0200

    added searchbar with filter method

---
+++ searchbar.js
@@ -0,0 +1,38 @@
+// algorithm with method filter
+
+// using listOfRecipes
+// includes name or description or ingredients
+// length >2 tolowercase()
+// trim() before to avoid spaces
+// TO DO disable the cross in the search input with css 
+
+// search function with .filter method, reusing displayAvailableRecipes() from the main.js
+const inputSearchBar = document.querySelector('#search-input')
+inputSearchBar.addEventListener('keyup', (e)=>{
+    const searchValue = e.target.value.toLowerCase().trim();
+    if(searchValue.length>2){
+        availableListOfRecipes = listOfRecipes.filter((oneRecipe)=>{
+            //include ... OR ...
+            return (oneRecipe.name.toLowerCase().includes(searchValue) || oneRecipe.description.toLowerCase().includes(searchValue)
+            || oneRecipe.ingredients.some((oneIngredient)=> oneIngredient.name.toLowerCase().includes(searchValue))) 
+        })
+        removeTags(); // whenever there is a new seach starting
+        displayAvailableRecipes();
+    } else if (searchValue.length<1) {
+        restart()
+    }
+})
+
+// function which allow to start again a new search with no settings(no tags, display the full list of recipes)
+function restart(){
+    removeTags()
+    availableListOfRecipes = [...listOfRecipes];
+    displayAvailableRecipes()
+}
+
+function removeTags(){
+    containerTag.innerHTML="";
+    ingredientsActiveTags = [];
+    appliancesActiveTags = [];
+    utensilsActiveTags = [];
+}
